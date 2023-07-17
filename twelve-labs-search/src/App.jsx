ad8f662caf902a11105010e15452a62dbe08f28a
Travis Couture <travis.m.couture@gmail.com>
2023-07-17 22:55:26 +0000

    Themeing and render main controls.

--- twelve-labs-search/src/App.jsx
+++ twelve-labs-search/src/App.jsx
@@ -1,13 +1,34 @@
 import { useState } from 'react'
 import './App.css'
-import fs from 'fs'
-import ytdl from 'ytdl-core';
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import UploadYoutubeVideo from './UploadYoutubeVideo'
+import Grid from '@mui/material/Unstable_Grid2/'
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#b4f464',
+    },
+    secondary: { 
+      main: '#fc8c3c' 
+    }
+  }
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [indexID, setIndexID] = useState()
+  const [indexedVideos, setIndexedVideos] = useState()
 
   return (
     <>
+      <ThemeProvider theme={theme}>
+        <Grid display='flex' justifyContent='center' alignItems='center' minHeight='95vh' container>
+          <UploadYoutubeVideo indexedVideos={ indexedVideos } setIndexedVideos={ setIndexedVideos }/>
+        </Grid>
+        <CssBaseline />
+      </ThemeProvider>
     </>
   )
 }
